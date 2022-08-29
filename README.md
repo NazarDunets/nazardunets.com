bb69121a1f08a1e402f65c2ebd018dbd92c8574b
NazarDunets <jcq.nazardunets@gmail.com>
2022-08-29 17:46:59 +0300

    basic website

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple person web page
+
+Svelte is completely unnecessary here, but I will add something to justify it's usage in future.
---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import SvgIcon from "@jamescoyle/svelte-icon";
+  import androidLogo from "./assets/android_jetpack.svg";
+  import kotlinLogo from "./assets/kotlin.svg";
+  import { mdiSend, mdiGithub, mdiLinkedin, mdiEmail } from "@mdi/js";
+</script>
+
+<main>
+  <div>
+    <img src={kotlinLogo} class="logo kotlin" alt="Kotlin Logo" />
+    <img src={androidLogo} class="logo android" alt="Android Logo" />
+  </div>
+
+  <h1>Nazar Dunets</h1>
+  <h3>Android Developer</h3>
+
+  <div class="row contact" style="padding-bottom: 1em; padding-top: 1em">
+    <SvgIcon type="mdi" path={mdiEmail} />
+    <div style="padding-left: 0.5em">jcq.nazardunets@gmail.com</div>
+  </div>
+
+  <div class="row">
+    <a href="https://www.linkedin.com/in/nazar-dunets/" class="contact">
+      <SvgIcon type="mdi" path={mdiLinkedin} />
+    </a>
+    <a href="https://t.me/jcqNazar" class="contact">
+      <SvgIcon type="mdi" path={mdiSend} />
+    </a>
+    <a href="https://github.com/NazarDunets" class="contact">
+      <SvgIcon type="mdi" path={mdiGithub} />
+    </a>
+  </div>
+</main>
