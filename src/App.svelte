608716ef06319fdd1ff3ca0b1b8811137d78d740
NazarDunets <jcq.nazardunets@gmail.com>
2022-08-30 00:12:51 +0300

    open links in new tab. no hover effect for email

--- src/App.svelte
+++ src/App.svelte
@@ -14,19 +14,19 @@
   <h1>Nazar Dunets</h1>
   <h3>Android Developer</h3>
 
-  <div class="row contact" style="padding-bottom: 1em; padding-top: 1em">
+  <div class="row email" style="padding-bottom: 1em; padding-top: 1em">
     <SvgIcon type="mdi" path={mdiEmail} />
     <div style="padding-left: 0.5em">jcq.nazardunets@gmail.com</div>
   </div>
 
   <div class="row">
-    <a href="https://www.linkedin.com/in/nazar-dunets/" class="contact">
+    <a href="https://www.linkedin.com/in/nazar-dunets/" target="_blank" class="contact">
       <SvgIcon type="mdi" path={mdiLinkedin} />
     </a>
-    <a href="https://t.me/jcqNazar" class="contact">
+    <a href="https://t.me/jcqNazar" target="_blank" class="contact">
       <SvgIcon type="mdi" path={mdiSend} />
     </a>
-    <a href="https://github.com/NazarDunets" class="contact">
+    <a href="https://github.com/NazarDunets" target="_blank" class="contact">
       <SvgIcon type="mdi" path={mdiGithub} />
     </a>
   </div>
